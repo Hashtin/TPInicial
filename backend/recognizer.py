35c2bc08b9d9fc338af4bd264cc27973dc76f0a8
FranSaMa373 <sanmartinfrancisco373@gmail.com>
2025-09-01 20:56:08 -0300

    Recognizer completo - gitignore modelo

--- backend/recognizer.py
+++ backend/recognizer.py
@@ -1,16 +1,21 @@
 import cv2
 import os
 import imutils 
+import numpy as np
 
-def capturar_rostro():
-    id_empleado = 0
-    ruta_data = "../dataset"
-    ruta_empleado = ruta_data + "/" + str(id_empleado)
-    ruta_video = ruta_data + "/videos/" + str(id_empleado) + ".mp4"
+RUTA_DATA = "../dataset"
+
+def capturar_rostro(id_empleado):
+    ruta_empleado = RUTA_DATA + "/" + str(id_empleado)
+    ruta_video = RUTA_DATA + "/videos/" + str(id_empleado) + ".mp4"
 
     if not os.path.exists(ruta_empleado):
         print('Carpeta creada: ', ruta_empleado)
         os.makedirs(ruta_empleado)
+    
+    if not os.path.exists(ruta_video):
+        print('NO SE ENCONTRO EL VIDEO DEL EMPLEADO')
+        return
     
     cap = cv2.VideoCapture(ruta_video)
 
@@ -39,3 +44,38 @@ def capturar_rostro():
             break
     cap.release()
     cv2.destroyAllWindows()
+
+def creacion_modelo():
+    lista_empleados = os.listdir(RUTA_DATA)
+
+    etiquetas = []
+    rostros_data = []
+
+    for id_dir in lista_empleados:
+        etiqueta = int(id_dir)
+        ruta_empleado = RUTA_DATA + "/" + str(id_dir)
+        print("Leyendo las imagenes")
+
+        for nombre_archivo in os.listdir(ruta_empleado):
+            print("Rostros: ", id_dir + '/' + nombre_archivo)
+            etiquetas.append(etiqueta)
+            rostros_data.append(cv2.imread(ruta_empleado + "/" + nombre_archivo,0))
+            ##Representativo
+            image = cv2.imread(ruta_empleado + "/" + nombre_archivo,0)
+            cv2.imshow('image', image)
+            cv2.waitKey(10)
+
+
+    print("numero de etiquetas 0: ", np.count_nonzero(np.array(etiquetas)==0))
+    print("numero de etiquetas 1: ", np.count_nonzero(np.array(etiquetas)==1))
+    print("numero de etiquetas 2: ", np.count_nonzero(np.array(etiquetas)==2))
+    print("numero de etiquetas 3: ", np.count_nonzero(np.array(etiquetas)==3))
+    
+    entrenamiento_eigen_face(rostros_data,etiquetas)
+
+def entrenamiento_eigen_face(rostros_data,etiquetas):
+    face_recognizer = cv2.face.EigenFaceRecognizer_create()
+    print("Entrenando...")
+    face_recognizer.train(rostros_data, np.array(etiquetas))
+    face_recognizer.write("modeloEigenFaces.xml")
+    print("Modelo almacenado")
